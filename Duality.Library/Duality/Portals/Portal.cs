978905e7502af4eadbbed9c648b54b3297884e9a
Jacob Penner <jkpenner@live.com>
2022-01-17 21:20:10 -0800

    Update basic portal script to affect World changing

--- Duality.Library/Duality/Portals/Portal.cs
+++ Duality.Library/Duality/Portals/Portal.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Duality
@@ -5,7 +6,77 @@ namespace Duality
     [AddComponentMenu("Duality/Portals/Portal")]
     public class Portal : MonoBehaviour
     {
+        [SerializeField] Portal connectedPortal = null;
+        [SerializeField] World world = World.One;
+
         public bool IsPlaced { get; private set; }
         public Renderer Renderer { get; private set; }
+
+        private List<GameObject> objects = new List<GameObject>();
+
+        public Portal ConnectedPortal => connectedPortal;
+        public World World => world;
+
+        async void OnTriggerEnter(Collider other)
+        {
+            if (objects.Contains(other.gameObject))
+            {
+                return;
+            }
+
+            // Flip objects current world if a world object
+            var worldObject = other.GetComponent<IWorldObject>();
+            if (worldObject is null)
+            {
+                return;
+            }
+
+            Debug.Log($"Teleported {other.name}");
+            
+            // Transform offset and forward direction relative to the in portal.
+            var offset = transform.InverseTransformPoint(other.transform.position);
+            var forward = transform.InverseTransformDirection(other.transform.forward);
+
+
+            // Transform offset and forward direction relative to the out portal.
+            var newOffset = connectedPortal.transform.TransformPoint(offset);
+            var newForward = connectedPortal.transform.TransformDirection(forward);
+            newForward.x *= -1f;
+            newForward.z *= -1f;
+
+            var newRotation = Quaternion.LookRotation(newForward, connectedPortal.transform.up);
+
+            Debug.DrawRay(transform.position, transform.forward, Color.red, 1f);
+            Debug.DrawRay(transform.position, newForward, Color.red, 1f);
+
+            worldObject.FlipWorld();
+            worldObject.WrapPosition(newOffset, newRotation);
+
+            connectedPortal.ReceiveObject(other.gameObject);
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            objects.Remove(other.gameObject);
+        }
+
+        public void ReceiveObject(GameObject other)
+        {
+            this.objects.Add(other);
+        }
+
+        void OnDrawGizmos()
+        {
+            if (connectedPortal is null)
+            {
+                return;
+            }
+
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(
+                transform.position + Vector3.up * 2f,
+                connectedPortal.transform.position + Vector3.up * 2f
+            );
+        }
     }
 }
